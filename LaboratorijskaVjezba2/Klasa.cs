cb5633bee23f5821109aca049d1534d8b6f85953
Kata Pogacic <KPogacic@dspace.hr>
2021-10-21 18:16:22 +0200

    Add project files.

---
+++ LaboratorijskaVjezba2/Klasa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorijskaVjezba2
+{
+    class KomplekniBrojKlasa
+    {
+        public KomplekniBrojKlasa(double realni, double imaginarni)
+        {
+            RealniDio = realni;
+            ImaginarniDio = imaginarni;
+        }
+        public double RealniDio;
+        public double ImaginarniDio;
+
+        public override string ToString()
+        {
+            return $"({RealniDio},{ImaginarniDio})";//sve u zagradama su varijable
+        }
+    }
+
+}
---
+++ LaboratorijskaVjezba2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorijskaVjezba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KomplekniBrojKlasa kbk1 = new KomplekniBrojKlasa(2, 4);
+            KomplekniBrojStruktura kbs1 = new KomplekniBrojStruktura(3, 5);
+
+            KomplekniBrojKlasa kbk2 = kbk1;
+            KomplekniBrojStruktura kbs2 = kbs1;
+
+            Console.WriteLine($"kbk1={kbk1}");//poziva ToString metodu
+            Console.WriteLine($"kbk2={kbk2}");
+            Console.WriteLine($"kbs1={kbs1}");
+            Console.WriteLine($"kbs2={kbs2}");
+
+            kbk2.RealniDio = 25;
+            kbs2.ImaginarniDio = 38;
+
+            Console.WriteLine($"kbk1={kbk1}");//poziva ToString metodu
+            Console.WriteLine($"kbk2={kbk2}");
+            Console.WriteLine($"kbs1={kbs1}");
+            Console.WriteLine($"kbs2={kbs2}");
+
+            Console.ReadKey(false);
+
+
+        }
+    }
+}
